545520bd7cbd2e6ffe6c38323a903093fefdd1d7
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2023-02-17 09:55:20 +0630

    a

---
+++ wlk-online-shop/admin/layouts_copy.php
@@ -0,0 +1,43 @@
+<?php 
+
+include "layouts/head.php";
+
+
+?>
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern 2-columns   fixed-navbar" data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+
+<?php 
+
+include "layouts/navbar.php";
+include "layouts/sidebar.php";
+
+?>
+
+
+    
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <!-- eCommerce statistic -->
+                
+                <!--/ Basic Horizontal Timeline -->
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+<?php 
+include "layouts/footer.php";
+include "layouts/scripts.php";
+
+?>
